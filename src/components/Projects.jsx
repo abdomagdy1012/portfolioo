7f21d4493c4c7728c321bddb9a577a9f1c5b5ac0
abdomagdy1012 <abdomagdy888.aa@gmail.com>
2026-05-18 16:04:57 +0300

    updated files

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,71 +1,59 @@
-// import { motion } from "framer-motion";
-
-// export default function Projects() {
-//   return (
-//     <section id="projects" className="px-6 pb-20 max-w-6xl mx-auto">
-//       <h2 className="text-3xl font-bold text-center mb-10">Projects</h2>
-
-//       <div className="grid md:grid-cols-3 gap-6">
-
-//         <motion.div
-//             whileHover={{ scale: 1.05 }}
-//             className="group bg-white/5 backdrop-blur-lg border border-white/10 rounded-2xl overflow-hidden hover:shadow-purple-500/20 transition-all duration-300 ease-out"
-//             >
-//           {/* Image */}
-//           <div className="relative overflow-hidden">
-//             <img
-//               src="/project.png"
-//               className="w-full h-40 object-cover group-hover:scale-110 transition duration-500"
-//             />
-
-//             {/* Overlay */}
-//             <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition flex items-center justify-center gap-3">
-//               <a className="px-3 py-1 bg-purple-600 rounded-lg text-sm">Live</a>
-//               <a className="px-3 py-1 border border-white/20 rounded-lg text-sm">GitHub</a>
-//             </div>
-//           </div>
-
-//           {/* Content */}
-//           <div className="p-5">
-//             <h3 className="text-xl font-semibold mb-2">
-//               E-commerce App
-//             </h3>
-
-//             <p className="text-white/60 text-sm">
-//               React app with authentication, cart and API integration.
-//             </p>
-//           </div>
-//         </motion.div>
-
-//       </div>
-//     </section>
-//   );
-// }
 import ProjectCard from "./ProjectCard";
 import { SectionWrapper } from "./SectionWrapper";
 
 const projects = [
   {
-    title: "FreshCart Website",
-    desc: "Full-featured React e-commerce application with secure authentication (register/login), cart management, and a complete checkout flow.",
-    image: "images/FreshCartimages/Wallpaper.png",
-    live: "https://your-live-site.com",
-    github: "https://github.com/abdomagdy1012/FreshCart",
-  },
-  {
     title: "Amazon Website",
-    desc: "A responsive Amazon-inspired e-commerce website built with HTML, CSS, and JavaScript, designed to replicate real-world shopping UI and user experience.",
-    image: "/project2.png",
-    live: "https://your-live-site.com",
+    desc: `Developed a responsive e-commerce web application inspired by Amazon using modern front-end technologies.
+        Implemented product listing, cart system, and state management with dynamic UI updates.
+        Focused on clean design, user experience, and scalable component-based architecture.`,
+    image: "images/Amazon/checkout.png",
+    live: "https://amazonproject-lyart.vercel.app/",
     github: "https://github.com/abdomagdy1012/Amazon-Project",
   },
   {
     title: "Bird Game",
-    desc: "Modern animated portfolio built with React & Tailwind.",
-    image: "images/birdGame/start.png",
+    desc: `Built an interactive browser-based game using vanilla JavaScript with dynamic object generation and collision logic.
+    Implemented scoring system, timer, and moving obstacles (birds and bombs) using DOM manipulation and setInterval.
+            Focused on game mechanics, responsiveness, and real-time user interaction.`,
+    image: "images/BirdGame/start.PNG",
     live: "https://birdgame-kappa.vercel.app/",
     github: "https://github.com/abdomagdy1012/BirdGame",
   },
+   {
+    title: "Movies Website",
+    desc: `Built a responsive single-page movie browsing app using Angular CLI with REST API integration.
+    Implemented routing, reusable components, and dynamic search and details views.
+    Focused on clean UI, performance, and modular TypeScript architecture.`,
+    image: "images/Moviesite/HomePage.PNG",
+    live: "https://movie-site-six-teal.vercel.app/  ",
+    github: "https://github.com/abdomagdy1012/MovieSite",
+  },
+   {
+    title: "FreshCart Website",
+    desc: `Built a responsive e-commerce web application using React with dynamic product listing and cart functionality.
+      Integrated REST APIs for real-time data handling and managed state using React hooks.
+      Focused on reusable components, performance optimization, and clean UI/UX design.`,
+    image: "images/FreshCartimages/Wallpaper.png",
+    github: "https://github.com/abdomagdy1012/FreshCart",
+  },
+  {
+    title: "Food-Ordering Website",
+    desc: `A responsive web application that allows users to browse restaurants, view menus, and place food orders seamlessly.
+        Integrated REST APIs for dynamic data handling with features like authentication, cart management, and order processing.
+        Built with a modular architecture focusing on performance, usability, and clean UI/UX design.`,
+    image: "images/foodordering/login.PNG",
+    github: "https://github.com/Mohamedkh30/FoodOrderSystemAPI",
+  },
+  {
+    title: "WareHouse Project",
+    desc: `Built a responsive single-page movie browsing app using Angular CLI with REST API integration.
+    Implemented routing, reusable components, and dynamic search and details views.
+    Focused on clean UI, performance, and modular TypeScript architecture.`,
+    image: "images/Moviesite/HomePage.PNG",
+    github: "https://github.com/abdomagdy1012/MovieSite",
+  },
+
 ];
 
 export default function Projects() {
@@ -77,7 +65,7 @@ export default function Projects() {
         My Projects
       </h2>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         {projects.map((project, index) => (
           <ProjectCard key={index} project={project} />
         ))}
